b92030671702f38c6f23ef1bf0454ce3d261882e
MoJafa <jmohsa@gmail.com>
2023-02-25 10:48:07 +0200

    commit

--- week_2_workflow_orchestration/homework/question_four/etl_web_to_gcs.py
+++ week_2_workflow_orchestration/homework/question_four/etl_web_to_gcs.py
@@ -1,38 +1,40 @@
-import pandas as pd
 from pathlib import Path
-from datetime import timedelta
+import pandas as pd
 from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
-# from prefect.tasks import task_input_hash
-# from random import randint
+from random import randint
 
-# For some reason, the commented @task below is responsible for an exception when deploying the code using
-# the docker block as the infrastructure. It has something to do with cache_key_fn and cache_expiration.
-# I discovered this solution by reading a thread in the course's Slack.
-# See:
-# https://datatalks-club.slack.com/archives/C01FABYF2RG/p1674823816614039
-# https://github.com/PrefectHQ/prefect/issues/6086
-# @task(retries=3, cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1))
+def convert_dtype(x):
+    """Convert a column as string
+    This code comes from
+    https://www.roelpeters.be/solved-dtypewarning-columns-have-mixed-types-specify-dtype-option-on-import-or-set-low-memory-in-pandas/
+    """
+    if not x:
+        return ''
+    try:
+        return x.astype(str)
+    except:
+        return ''
+
 @task(retries=3)
 def fetch(dataset_url: str) -> pd.DataFrame:
     """Read taxi data from web into pandas DataFrame"""
-    # simulating failure to test retries
-    # if randint(0, 1) == 1:
-    #     raise Exception()
     df = pd.read_csv(dataset_url)
+
+    print(df.info(verbose=True))
     return df
 
 
-
 @task(log_prints=True)
-def clean(df: pd.DataFrame, color: str) -> pd.DataFrame:
+def clean(df: pd.DataFrame) -> pd.DataFrame:
     """Fix dtype issues"""
-    if color == "yellow":
+    if "tpep_pickup_datetime" in df.columns:
         df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"])
         df["tpep_dropoff_datetime"] = pd.to_datetime(df["tpep_dropoff_datetime"])
     else:
         df["lpep_pickup_datetime"] = pd.to_datetime(df["lpep_pickup_datetime"])
         df["lpep_dropoff_datetime"] = pd.to_datetime(df["lpep_dropoff_datetime"])
+
     print(df.head(2))
     print(f"columns: {df.dtypes}")
     print(f"rows: {len(df)}")
@@ -42,12 +44,13 @@ def clean(df: pd.DataFrame, color: str) -> pd.DataFrame:
 @task()
 def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
     """Write DataFrame out locally as parquet file"""
+    Path(f"data/{color}").mkdir(parents=True, exist_ok=True)
     path = Path(f"data/{color}/{dataset_file}.parquet")
     df.to_parquet(path, compression="gzip")
     return path
 
 
-@task()
+@task
 def write_gcs(path: Path) -> None:
     """Upload local parquet file to GCS"""
     gcs_block = GcsBucket.load("dezoomcamp")
@@ -56,29 +59,20 @@ def write_gcs(path: Path) -> None:
 
 
 @flow()
-def etl_web_to_gcs(year: int, month: int, color: str) -> None:
+def etl_web_to_gcs() -> None:
     """The main ETL function"""
+    color = "green"
+    year = 2020
+    month = 11
+
     dataset_file = f"{color}_tripdata_{year}-{month:02}"
     dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
 
     df = fetch(dataset_url)
-    df_clean = clean(
-        df, color
-    )  # add color so can fix different column in green dataset
+    df_clean = clean(df)
     path = write_local(df_clean, color, dataset_file)
     write_gcs(path)
 
 
-@flow()
-def etl_parent_flow(
-    months: list[int] = [1, 2], year: int = 2021, color: str = "yellow"
-):
-    for month in months:
-        etl_web_to_gcs(year, month, color)
-
-
-if __name__ == '__main__':
-    color = 'green'
-    months = [11]
-    year = 2020
-    etl_parent_flow(months, year, color)
+if __name__ == "__main__":
+    etl_web_to_gcs()
