a63dc7ee1f8c90d7ecc88777f9fb03633ab12431
MoJafa <jmohsa@gmail.com>
2023-03-07 22:58:07 +0200

    added download data script

---
+++ week_5_batch_processing/code/download_data.sh
@@ -0,0 +1,28 @@
+set -e
+
+URL_PREFIX="https://github.com/DataTalksClub/nyc-tlc-data/releases/download"
+
+for TAXI_TYPE in "yellow" "green"
+do
+    for YEAR in 2020 2021
+    do
+        for MONTH in {1..12}
+        do
+
+        if [ $YEAR == 2020 ] || [ $MONTH -lt 8 ]
+        then
+            FMONTH=`printf "%02d" ${MONTH}`
+
+            URL="${URL_PREFIX}/${TAXI_TYPE}/${TAXI_TYPE}_tripdata_${YEAR}-${FMONTH}.csv.gz"
+
+            LOCAL_PREFIX="data/raw/${TAXI_TYPE}/${YEAR}/${FMONTH}"
+            LOCAL_FILE="${TAXI_TYPE}_tripdata_${YEAR}_${FMONTH}.csv.gz"
+            LOCAL_PATH="${LOCAL_PREFIX}/${LOCAL_FILE}"
+
+            echo "donwloading ${URL} to ${LOCAL_PATH}"
+            mkdir -p ${LOCAL_PREFIX}
+            wget ${URL} -O ${LOCAL_PATH}
+        fi
+        done
+    done
+done
